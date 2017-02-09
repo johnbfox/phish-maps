3ab675d4adf626e050a8848e203e1cb6019fbd81
John Fox <johnfox@Johns-MacBook-Pro.local>
2017-02-09 15:12:30 -0500

    Self reference endpoint

--- js/script.js
+++ js/script.js
@@ -59,10 +59,10 @@ function intializeGeoChart(){
 
 function mapForYear(year){
   var drawRegionsMap = function() {
-    var url= 'http://johnbfox.com:5000/phish-api/showStateCountCum'
+    var url= '/phish-api/showStateCountCum'
 
     if(regionsMode === '0'){
-      url = 'http://johnbfox.com:5000/phish-api/showStateCount';
+      url = '/phish-api/showStateCount';
     }
 
     url = url + '/' + year;
